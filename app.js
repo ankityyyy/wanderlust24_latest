00ce156f83f2f630989c95129549db31cc261883
Mohammad Amaan <mohammadamaanx@gmail.com>
2024-06-04 16:23:58 +0530

    Made changes in app.js, review controller, review eouter and show.ejs

--- app.js
+++ app.js
@@ -1,59 +1,58 @@
-const express=require("express");
-const app=express();
-const mongoose = require('mongoose');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const Listing = require("./models/listings.js");
-const path=require("path");
-const methodOverride = require('method-override');
-const ejsMate = require('ejs-mate');
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
-const ExpressError=require("./utils/ExpressError.js")
-const {listingSchema,reviewSchema}= require("./schema.js");
-const Review=require("./models/reviews.js");
-const listingRouter=require("./routes/listings.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/reviews.js");
+const listingRouter = require("./routes/listings.js");
 const reviewRouter = require("./routes/reviews.js");
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport=require('passport')
-const  LocalStrategy=require('passport-local');
-const User=require("./models/users.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/users.js");
 const userRouter = require("./routes/users.js");
 
-
 main()
-.then(()=>{
+  .then(() => {
     console.log("connection successful");
-})
-.catch(err => console.log(err));
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wanderfirst'); 
-};
+  await mongoose.connect("mongodb://127.0.0.1:27017/wanderfirst");
+}
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.set("view engine","ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
-app.engine('ejs', ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("hi i am root");
-})
+});
 
+app.use(
+  session({
+    secret: "mysupersecretstring",
+    resave: "false",
+    saveUninitialized: "true",
+    Cookie: {
+      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+      httpOnly: true,
+    },
+  })
+);
 
-app.use(session({secret:"mysupersecretstring" ,
-              resave:"false",
-              saveUninitialized :"true",
-              Cookie:{
-                expires:Date.now()+7*24*60*60*1000,
-                maxAge:7*24*60*60*1000,
-                httpOnly:true,
-              }
-}))
-
-app.use(flash())
+app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -62,51 +61,38 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req, res, next) => {
+  res.locals.message = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.CurrUser = req.user;
+  next();
+});
 
-app.use((req,res,next)=>{
-  res.locals.message=req.flash("success")
-  res.locals.error=req.flash("error")
-  res.locals.CurrUser=req.user;
-  next()
-})
-
-app.get("/demouser",async(req,res)=>{
-  let fakeData=new User({
-    email:"ankit@123789",
-    phone:620582488998,
-    username:"ankit",
-  })
-  let resa=await User.register(fakeData,"ankit")
+app.get("/demouser", async (req, res) => {
+  let fakeData = new User({
+    email: "ankit@123789",
+    phone: 620582488998,
+    username: "ankit",
+  });
+  let resa = await User.register(fakeData, "ankit");
   res.send(resa);
-})
+});
 
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
+app.use("*", (req, res, next) => {
+  next(new ExpressError(404, "page not found"));
+});
 
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "something went wrongs" } = err;
+  console.log(err);
+  res.status(statusCode).render("error.ejs", { err });
+  //res.status(statusCode).send(message);
+});
 
-app.use("/listings",listingRouter);
-app.use("/listings/:id/reviews",reviewRouter)
-app.use("/",userRouter)
-
-
-
-
-
-
-app.use("*",(req,res,next)=>{
-  next(new ExpressError(404,"page not found"));
-  })
-  
-
-app.use((err,req,res,next)=>{
-  let{statusCode=500,message="something went wrongs"}=err;
-  res.status(statusCode).render("error.ejs",{err});
-//res.status(statusCode).send(message);
-})
-
-
-app.listen(8080,()=>{
-    console.log("listen on port number:8080")
-}
-)
-
-
+app.listen(8080, () => {
+  console.log("listen on port number:8080");
+});
--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,25 +1,22 @@
 const Listing = require("../models/listings.js");
 const Review = require("../models/reviews.js");
 
+module.exports.reviewCreated = async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  console.log(req.body);
+  let newReview = new Review(req.body.review);
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
+  req.flash("success", "new reviews created!");
+  res.redirect(`/listings/${listing._id}`);
+};
 
-module.exports.reviewCreated=async(req,res)=>{
-    let listing =await Listing.findById(req.params.id);
-    console.log(req.body);
-    let newReview=new Review(req.body.review)
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success","new reviews created!");
-    res.redirect(`/listings/${listing._id}`)
-  }
-
-
-  module.exports.reviewDelete=async(req,res)=>{
-  
-    let { id, reviewId } = req.params;
-        await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); //pull is used to remove items
-        await Review.findByIdAndDelete(reviewId);
-        console.log("revied deleted");
-        req.flash("error","Delete reviews ");
-        res.redirect(`/listing/${id}`); 
-  }
+module.exports.reviewDelete = async (req, res) => {
+  let { id, reviewId } = req.params;
+  console.log(req.params);
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); //pull is used to remove items
+  await Review.findByIdAndDelete(reviewId);
+  console.log("revied deleted");
+  res.redirect(`/listings/${id}`);
+};
