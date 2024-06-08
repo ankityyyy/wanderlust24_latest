1754709199800724605a72072ed059b5106238f7
ankit dubey <ankitdubey9431969639@gmail.com>
2024-06-08 14:51:37 +0530

    Corrected wrapAsync and ExpressError

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const wrapAsync = require("../utils/wrapasync.js");
-const ExpressError = require("../utils/Expresserror.js");
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/reviews.js");
 const Listing = require("../models/listings.js");
 const { validateReview } = require("../middleware.js");
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 const user=require('../models/users.js')
-const wrapAsync=require("../utils/wrapasync.js");
+const wrapAsync=require("../utils/wrapAsync.js");
 const passport = require("passport");
 const {saveRedirectUrl}=require("../middleware.js")
 const userController=require("../controllers/users.js");
